3efff38e5b5b9499a3411f6db6c6a7eb23f22510
Brandon <73324449+BrandonW-ERAU@users.noreply.github.com>
2020-10-26 19:55:58 -0700

    Dices do not have 7 sides. Fixed that. Put everything in less lines.

--- functionhw.py
+++ functionhw.py
@@ -1,19 +1,10 @@
 import random
 def roll_dice(num_dice):
   dice_rolls = []
-  int(num_dice)
   for y in range(num_dice):
-    dice_rolls.append(random.randint(1, 7))
+    dice_rolls.append(random.randint(1, 6))
     dice_rolls.sort()
   return dice_rolls
-
-print(roll_dice(6))
-
+print(roll_dice(30))
 roll = roll_dice
-
 print(roll(4))
-
-
-
-
-
