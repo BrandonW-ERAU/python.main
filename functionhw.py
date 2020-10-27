0a5a6a6cc086f46b75e48fcda85e711d84a9f868
Brandon <73324449+BrandonW-ERAU@users.noreply.github.com>
2020-10-26 21:37:14 -0700

    YAHTZEE

--- functionhw.py
+++ functionhw.py
@@ -1,16 +1,27 @@
 from random import randint
+import sys
+sys.setrecursionlimit(999999)
 def roll_dice(num_dice):
-  # Takes in number of dice to roll.
+  yahtzee = []
   dice_rolls = []
+  fails = 0
+  fails = fails + 1
   for y in range(num_dice):
     dice_rolls.append(randint(1, 6))
     dice_rolls.sort()
-  # For loop rolls each dice randomly.
-  return dice_rolls
-  # return allows the list of dice rolls to be printed.
-print(roll_dice(10))
-# The sets the amount of dice to be rolled.
-roll = roll_dice
-# Demonstrates high order functions
-print(roll(4))
-# This will print the result of 4 dice rolls.
+    print("Fail", end=' ')
+  if dice_rolls[0] != dice_rolls[4]:
+    roll_dice(num_dice)
+  elif dice_rolls[0] == dice_rolls[4]:
+    print("\nyahtzee!!!")
+    yahtzee.append(tuple(dice_rolls))
+    print(yahtzee)
+  return 2
+roll_dice(5)
+
+
+
+
+
+
+
