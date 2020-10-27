b851435ab755f66c86988bcff51b275f549c2aed
Brandon <73324449+BrandonW-ERAU@users.noreply.github.com>
2020-10-26 19:48:34 -0700

    The dice rolls are now sorted and in a list.

--- functionhw.py
+++ functionhw.py
@@ -1,11 +1,19 @@
 import random
 def roll_dice(num_dice):
+  dice_rolls = []
   int(num_dice)
   for y in range(num_dice):
-    dice = random.randint(1, 7)
-    print(dice)
+    dice_rolls.append(random.randint(1, 7))
+    dice_rolls.sort()
+  return dice_rolls
 
-roll_dice(8)
+print(roll_dice(6))
+
+roll = roll_dice
+
+print(roll(4))
+
+
 
 
 
